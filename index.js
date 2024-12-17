7cb1efb8e68ce78e380185f617dd39b63946194b
Abhishek parihar <114382172+abhipariharr@users.noreply.github.com>
2024-12-17 15:23:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ function clearDisplay(){
 }
 function Calculate(){
     try{
-        display.value = eval(display.value);
-        // display.value = "I Love You";
+      //   display.value = eval(display.value);
+        display.value = "I Love You";
     }
     catch(error){
         display.value = "Error"
